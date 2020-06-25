b7ba89588cd2c13f17f01dc4e29225dcae99726b
Daniel Ali <daniel.ray.ali@gmail.com>
2020-06-25 14:05:12 -0400

    Added arguments for flexibility
    - Included Usage
    
    - Implemented selection between file and standard input
    
    - Printing newlines if line is just whitespace
    
    - Adding checks for margin and input file

--- main.cc
+++ main.cc
@@ -20,13 +20,44 @@ void TrimWhitespace(string& line) {
     }
 }
 
+void Usage(char* prog) {
+    cout << "Usage: " << prog << " <margin> [input_file]" << endl;
+    cout << "<margin> is required. [input_file] is optional. If [input_file] unspecfied, default is standard input." << endl;
+}
+
 int main(int argc, char* argv[]) {
-    ifstream input("input.txt");
+    if (argc < 2) {
+        Usage(argv[0]);
+        return 1;
+    }
+
+    int margin = atoi(argv[1]);
+    if (margin == 0) {
+        cerr << "Failed to convert margin to non-zero number: " << argv[1] << endl;
+        Usage(argv[0]);
+        return 1;
+    }
+
+    istream* input;
+    ifstream file_stream;
+    if (argc == 3) {
+        file_stream.open(argv[2]);
+        if (!file_stream.good()) {
+            cerr << "Could not open input file: " << argv[2] << endl;
+            Usage(argv[0]);
+            return 1;
+        }
+        input = &file_stream;
+    } else {
+        input = &cin;
+    }
+
     string line;
-    int margin = 100;
-    while (getline(input, line)) {
+    while (getline(*input, line)) {
         TrimWhitespace(line);
-        if (line.size() >= margin) {
+        if (line.size() == 0) {
+            cout << endl;
+        } else if (line.size() >= margin) {
             cout << line << endl;
         } else {
             for (int i = 0; i < (margin - line.size()); ++i) {
